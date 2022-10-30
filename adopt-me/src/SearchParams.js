0556a5674a257c434fb2440993c85232b95c1174
Ash <01tripster@gmail.com>
2022-10-31 00:37:05 +0530

    mapping data to component

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,19 +1,71 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   // const location = "itarsi, india";
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
-          Location {location}
+          {/* Location {location} */}
+          Location
           <input
             id="loacation"
             value={location}
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
