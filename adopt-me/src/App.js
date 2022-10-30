6e823bf3231cebaceb78a614f5373790637066d6
Ash <01tripster@gmail.com>
2022-10-30 10:57:20 +0530

    parcel added in the project

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -27,4 +30,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
