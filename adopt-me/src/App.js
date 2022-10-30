dacababd808ab3913323352530ea0b730d4ac3e7
Ash <01tripster@gmail.com>
2022-10-30 12:13:32 +0530

    using JSX

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -3,24 +3,35 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt me"),
-    React.createElement(Pet, {
-      name: "luna",
-      animal: "dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "happy",
-      animal: "dog",
-      breed: "cocktiel",
-    }),
-    React.createElement(Pet, {
-      name: "oreo",
-      animal: "dog",
-      breed: "Normal",
-    }),
-  ]);
+  return (
+    <div id="my-app">
+      <h1>Adopt Me!</h1>
+      <Pet name="Julie" animal="dog" breed="mix" />
+      <Pet name="happy" animal="dog" breed="normal" />
+      <Pet name="oreo" animal="dog" breed="normal" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "brand" }, "Adopt me"),
+//     React.createElement(Pet, {
+//       name: "luna",
+//       animal: "dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "happy",
+//       animal: "dog",
+//       breed: "cocktiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "oreo",
+//       animal: "dog",
+//       breed: "Normal",
+//     }),
+//   ]);
+// };
+
+render(<App />, document.getElementById("root"));
--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,11 +1,19 @@
-import React from "react";
-
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.animal}</h1>
+      <h1>{props.breed}</h1>
+    </div>
+  );
 };
+
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+// };
 
 export default Pet;
