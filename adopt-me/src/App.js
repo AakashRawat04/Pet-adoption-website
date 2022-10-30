1331efd39897e38e4f4819922be98b32b0a65139
Ash <01tripster@gmail.com>
2022-10-30 11:28:45 +0530

    componet module -> creating a separate file for Pet component

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
