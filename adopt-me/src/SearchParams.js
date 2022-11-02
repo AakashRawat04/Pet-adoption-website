445c1fb65cb14993ba8a3c16623e99b8bfcc6278
Ash <01tripster@gmail.com>
2022-11-02 19:15:38 +0530

    custom hook implementation

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import useBreedList from "./useBreedList";
 import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -8,7 +9,8 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
+
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
