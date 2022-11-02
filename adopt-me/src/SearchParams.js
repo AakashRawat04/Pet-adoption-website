13aed6cfaf4dd1a863a025d9c058c1bc0bfdb162
Ash <01tripster@gmail.com>
2022-11-02 19:54:36 +0530

    handling user input

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -28,7 +28,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           {/* Location {location} */}
           Location
