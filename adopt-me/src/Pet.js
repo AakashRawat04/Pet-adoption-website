d25f73db581540d0426a5dce021497f6994553dd
Ash <01tripster@gmail.com>
2022-11-10 00:33:52 +0530

    style pet component

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,10 +1,21 @@
-const Pet = (props) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.animal}</h1>
-      <h1>{props.breed}</h1>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} — {breed} — {location}
+        </h2>
+      </div>
+    </a>
   );
 };
 
