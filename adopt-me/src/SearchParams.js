5240e438592509606458afbfdbdcf6e4efa50a2b
Ash <01tripster@gmail.com>
2022-11-27 18:23:59 +0530

    context and state

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -11,7 +11,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [breeds] = useBreedList(animal);
-  const [theme] = useContext(ThemeContext);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   const [pets, setPets] = useState([]);
 
@@ -89,6 +89,20 @@ const SearchParams = () => {
                 {breed}
               </option>
             ))}
+          </select>
+        </label>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Darkblue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="#f06d06">fog dog</option>
           </select>
         </label>
         <button style={{ backgroundColor: theme }}>Submit</button>
