fc117b8b72f76f7e3746c2f6ca1845476b41bfbc
Ash <01tripster@gmail.com>
2022-11-27 10:34:24 +0530

    fetching data in componentDidMount

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -8,6 +8,18 @@ class Details extends Component {
     this.state = { loading: true };
   }
 
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+    // this.setState(Object.assign({ loading: false }, json.pets[0]));
+    // this.setState({ loading: false });
+    // this.setState(json.pets[0]);
+  }
+
   render() {
     if (this.state.loading) {
       return <h2>loading...</h2>;
@@ -16,7 +28,7 @@ class Details extends Component {
     const { animal, breed, city, state, description, name } = this.state;
 
     return (
-      <div>
+      <div className="details">
         <div>
           <h1>{name}</h1>
           <h2>
@@ -30,4 +42,9 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
