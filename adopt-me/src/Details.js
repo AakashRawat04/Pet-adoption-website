77a65d14ced74fd61e33792f5712f5c63df9f0e0
Ash <01tripster@gmail.com>
2022-11-27 11:36:34 +0530

    managing state in class components

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   // constructor(props) {
@@ -27,10 +28,12 @@ class Details extends Component {
       return <h2>loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
