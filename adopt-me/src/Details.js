9b2531e0d82fe8230948969c0e7931c4980f87a8
Ash <01tripster@gmail.com>
2022-11-27 09:16:27 +0530

    class component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,8 +1,33 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
-};
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div>
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} — {breed} — {city} — {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
