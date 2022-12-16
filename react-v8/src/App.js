9124ece65d6868e691894547bb1faf957aca9f43
Ash <01tripster@gmail.com>
2022-12-16 19:32:11 +0530

    pass props to Pet component

--- react-v8/src/App.js
+++ react-v8/src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
 	return React.createElement("div", {}, [
-		React.createElement("h1", {}, "Luna"),
-		React.createElement("h2", {}, "Dog"),
-		React.createElement("h2", {}, "Havanese"),
+		React.createElement("h1", {}, props.name),
+		React.createElement("h2", {}, props.animal),
+		React.createElement("h2", {}, React.createElement("h2", {}, props.breed)),
 	]);
 };
 
 const App = () => {
 	return React.createElement("div", {}, [
 		React.createElement("h1", {}, "Adopt Me!"),
-		React.createElement(Pet),
-		React.createElement(Pet),
-		React.createElement(Pet),
+		React.createElement(Pet, {
+			name: "julie",
+			animal: "Dog",
+			breed: "Mix",
+		}),
+		React.createElement(Pet, {
+			name: "Happy",
+			animal: "Dog",
+			breed: "labrador",
+		}),
+		React.createElement(Pet, {
+			name: "Oreo",
+			animal: "Dog",
+			breed: "Landseer",
+		}),
 	]);
 };
 
