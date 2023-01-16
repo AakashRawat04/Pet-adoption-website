d8b5308aead75830122775c9b947a85b09161e23
Ash <01tripster@gmail.com>
2023-01-16 15:24:34 +0530

    component composition

--- react-v8/src/Pet.jsx
+++ react-v8/src/Pet.jsx
@@ -1,11 +1,24 @@
 const Pet = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
+	const { name, animal, breed, images, location, id } = props;
+
+	let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+	if (images.length) {
+		hero = images[0];
+	}
+
+	return (
+		<a href={`/{details}/${id}`} className="pet">
+			<div className="image-container">
+				<img src={hero} alt={name} />
+			</div>
+			<div className="info">
+				<h1>{name}</h1>
+				<h1>
+					`${animal} -- ${breed} -- ${location}`
+				</h1>
+			</div>
+		</a>
+	);
 };
 
 export default Pet;
--- react-v8/src/SearchParams.jsx
+++ react-v8/src/SearchParams.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -82,14 +82,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />;
     </div>
   );
 };
