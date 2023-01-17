8cd6c44adedaab3aae863444bb8c516991c9c937
Ash <01tripster@gmail.com>
2023-01-17 15:40:04 +0530

    change grammar in  Pet.jsx

--- react-v8/src/Pet.jsx
+++ react-v8/src/Pet.jsx
@@ -15,9 +15,7 @@ const Pet = (props) => {
 			</div>
 			<div className="info">
 				<h1>{name}</h1>
-				<h2>
-					{`${animal} -- ${breed} -- ${location}`}
-				</h2>
+				<h2>{`${animal} — ${breed} — ${location}`}</h2>
 			</div>
 		</Link>
 	);
