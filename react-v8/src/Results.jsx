db62579de72bafcd0fb5a1edb27e39e01ba8702b
Ash <01tripster@gmail.com>
2023-01-17 15:41:33 +0530

    fix minor bug in Results.jsx

--- react-v8/src/Results.jsx
+++ react-v8/src/Results.jsx
@@ -3,7 +3,7 @@ import Pet from "./Pet";
 const Results = ({ pets }) => {
 	return (
 		<div className="search">
-			{!pets.lenght ? (
+			{!pets.length ? (
 				<h1>No Pets Found</h1>
 			) : (
 				pets.map((pet) => {
