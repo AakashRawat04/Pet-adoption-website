43428b98042e03f92a6f4ade5b78214e21cd5e7e
Ash <01tripster@gmail.com>
2023-01-27 01:35:07 +0530

    tailwind basics

--- tailwind-with-reactjs-intermediate-react-v5/src/SearchParams.jsx
+++ tailwind-with-reactjs-intermediate-react-v5/src/SearchParams.jsx
@@ -20,8 +20,9 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
