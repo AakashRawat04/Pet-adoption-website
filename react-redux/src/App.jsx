fc05010031bcd5f260c6de1752c2ea5aa628cdf0
Ash <01tripster@gmail.com>
2023-01-28 19:42:44 +0530

    setup redux-toolkit

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-import AdoptedPetContext from "./AdoptedPetContext";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import { Provider } from "react-redux";
+import store from './store';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,11 +16,10 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
-  const adoptedPet = useState(null);
   return (
     <div>
       <BrowserRouter>
-        <AdoptedPetContext.Provider value={adoptedPet}>
+        <Provider store={store}>
           <QueryClientProvider client={queryClient}>
             <header>
               <Link to="/">Adopt Me!</Link>
@@ -30,7 +29,7 @@ const App = () => {
               <Route path="/" element={<SearchParams />} />
             </Routes>
           </QueryClientProvider>
-        </AdoptedPetContext.Provider>
+        </Provider>
       </BrowserRouter>
     </div>
   );
